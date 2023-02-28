023be6f611a788b88f43ec6becc8969f0e9cbe78
Zhihao Fu <fuzhihao@scutech.com>
2023-02-28 13:40:44 +0800

    complete tutorial04

--- tutorial04/leptjson.c
+++ tutorial04/leptjson.c
@@ -92,18 +92,57 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
 
 static const char* lept_parse_hex4(const char* p, unsigned* u) {
     /* \TODO */
+    int i;
+    char ch;
+    *u = 0;
+    for (i = 0; i < 4; i++) {
+        ch = *p++;
+        *u = *u << 4;
+        if (ch >= '0' && ch <= '9')
+            *u |= ch - '0';
+        else if (ch >= 'a' && ch <= 'f')
+            *u |= ch - 'a' + 10;
+        else if (ch >= 'A' && ch <= 'F')
+            *u |= ch -'A' + 10;
+        else
+            return NULL;
+    }
     return p;
 }
 
+/*
+| 码点范围            | 码点位数  | 字节1     | 字节2    | 字节3    | 字节4     |
+|:------------------:|:--------:|:--------:|:--------:|:--------:|:--------:|
+| U+0000 ~ U+007F    | 7        | 0xxxxxxx |
+| U+0080 ~ U+07FF    | 11       | 110xxxxx | 10xxxxxx |
+| U+0800 ~ U+FFFF    | 16       | 1110xxxx | 10xxxxxx | 10xxxxxx |
+| U+10000 ~ U+10FFFF | 21       | 11110xxx | 10xxxxxx | 10xxxxxx | 10xxxxxx |
+*/
 static void lept_encode_utf8(lept_context* c, unsigned u) {
     /* \TODO */
+    assert(u <= 0x10FFFF);
+    if (u <= 0x007F) {
+        PUTC(c, u & 0xFF);
+    } else if (u <= 0x07FF) {
+        PUTC(c, 0xC0 | ((u >> 6) & 0xFF));
+        PUTC(c, 0x80 | (u & 0x3F));
+    } else if (u <= 0xFFFF) {
+        PUTC(c, 0xE0 | ((u >> 12) & 0xFF));
+        PUTC(c, 0x80 | ((u >>  6) & 0x3F));
+        PUTC(c, 0x80 | (u & 0x3F));
+    } else {
+        PUTC(c, 0xF0 | ((u >> 18) & 0xFF));
+        PUTC(c, 0x80 | ((u >> 12) & 0x3F));
+        PUTC(c, 0x80 | ((u >> 6) & 0x3F));
+        PUTC(c, 0x80 | (u & 0x3F));
+    }
 }
 
 #define STRING_ERROR(ret) do { c->top = head; return ret; } while(0)
 
 static int lept_parse_string(lept_context* c, lept_value* v) {
     size_t head = c->top, len;
-    unsigned u;
+    unsigned u, low;
     const char* p;
     EXPECT(c, '\"');
     p = c->json;
@@ -129,6 +168,15 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
                         if (!(p = lept_parse_hex4(p, &u)))
                             STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_HEX);
                         /* \TODO surrogate handling */
+                        if (u >= 0xD800 && u <= 0xDBFF) {
+                            if (*p++ != '\\' || *p++ != 'u')
+                                STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE);
+                            if (!(p = lept_parse_hex4(p, &low)))
+                                STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_HEX);
+                            if (low < 0xDC00 || low > 0xDFFF)
+                                STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE);
+                            u = (((u - 0xD800) << 10) | (low - 0xDC00)) + 0x10000;
+                        }
                         lept_encode_utf8(c, u);
                         break;
                     default:
